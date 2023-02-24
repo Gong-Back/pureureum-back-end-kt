31ea84c5f0e136cc3d999e8fae45e37af1618c18
ycchoi <gks54410@gmail.com>
2023-02-24 16:02:56 +0900

    PRR-4 [build]: DB 설정 정보 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,8 +36,13 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+	// DB Dependency
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+
+	// Test Dependecny
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 }
