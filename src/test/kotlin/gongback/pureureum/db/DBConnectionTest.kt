a882840c1139a2ea16a6a21e4f213266ba051415
YC <gks54410@gmail.com>
2023-02-28 14:54:10 +0900

    PRR-11 [feat]: test 제거

--- src/test/kotlin/gongback/pureureum/db/DBConnectionTest.kt
+++ src/test/kotlin/gongback/pureureum/db/DBConnectionTest.kt
@@ -3,17 +3,13 @@ package gongback.pureureum.db
 import gongback.pureureum.domain.user.User
 import gongback.pureureum.domain.user.UserRepository
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
-import org.springframework.test.context.ActiveProfiles
 
-@DataJpaTest
-@ActiveProfiles("local")
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+// @DataJpaTest
+// @ActiveProfiles("local")
+// @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class DBConnectionTest @Autowired constructor(val userRepository: UserRepository) {
-    @Test
+    // @Test
     fun `DB 연결 테스트`() {
         // given
         val userName = "testUser"
