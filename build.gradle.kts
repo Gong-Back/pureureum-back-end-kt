58fae2ccfc0703307f913f3e4b7271b130a2bc4c
YC <gks54410@gmail.com>
2023-03-21 20:41:48 +0900

    PRR-22 [build]: Dependency 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
@@ -62,7 +63,8 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5-jvm:5.5.5")
     testImplementation("io.kotest:kotest-assertions-core-jvm:5.5.5")
     testImplementation("org.springframework.security:spring-security-test")
-    testImplementation("io.mockk:mockk:1.13.4")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
 
     // spring-rest-docs
     asciidoctorExt("org.springframework.restdocs:spring-restdocs-asciidoctor")
