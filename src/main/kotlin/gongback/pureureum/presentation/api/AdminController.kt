7c66129ce6b73bb9807d0bc093cd562552032de9
jiwon <ljwon77@daum.net>
2023-04-26 01:09:22 +0900

    PRR-57 [feat]: 시설 조회 및 업데이트 어드민 API 추가

---
+++ src/main/kotlin/gongback/pureureum/presentation/api/AdminController.kt
@@ -0,0 +1,69 @@
+package gongback.pureureum.presentation.api
+
+import gongback.pureureum.application.FacilityService
+import gongback.pureureum.application.dto.FacilityRes
+import gongback.pureureum.application.dto.FacilityWithDocIds
+import gongback.pureureum.domain.facility.FacilityProgress
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.ModelAndView
+import org.springframework.web.servlet.view.RedirectView
+
+@RestController
+@RequestMapping("/admin")
+class AdminController(
+    private val facilityService: FacilityService
+) {
+    @GetMapping
+    fun login(): String {
+        return "admin"
+    }
+
+    @GetMapping("/facility/download/{id}/{docId}")
+    fun downloadCertificationDoc(
+        @PathVariable("id") id: Long,
+        @PathVariable("docId") docId: Long
+    ): ModelAndView {
+        val downloadPath = facilityService.getCertificationDocDownloadPath(id, docId)
+        return ModelAndView(RedirectView(downloadPath))
+    }
+
+    @GetMapping("/facility/all")
+    fun getNotApprovedFacility(
+        @RequestParam("category") category: String
+    ): ResponseEntity<ApiResponse<List<FacilityRes>>> {
+        val facilityRes = facilityService.getNotApprovedFacilitiesByCategory(category)
+        return ResponseEntity.ok(ApiResponse.ok(facilityRes))
+    }
+
+    @GetMapping("/facility/{id}")
+    fun getFacility(
+        @PathVariable("id") id: Long
+    ): ResponseEntity<ApiResponse<FacilityWithDocIds>> {
+        val facilityRes = facilityService.getFacilityById(id)
+        return ResponseEntity.ok(ApiResponse.ok(facilityRes))
+    }
+
+    @PostMapping("/facility/update/{id}")
+    fun updateFacilityProgress(
+        @PathVariable("id") id: Long,
+        @RequestParam("progress") progress: FacilityProgress
+    ): ResponseEntity<Unit> {
+        facilityService.updateFacilityProgress(id, progress)
+        return ResponseEntity.ok().build()
+    }
+
+    @PostMapping("/facility/update")
+    fun updateFacilitiesProgress(
+        @RequestParam("ids") ids: List<Long>,
+        @RequestParam("progress") progress: FacilityProgress
+    ): ResponseEntity<Unit> {
+        facilityService.updateFacilitiesProgress(ids, progress)
+        return ResponseEntity.ok().build()
+    }
+}
--- src/test/kotlin/gongback/pureureum/application/FacilityServiceTest.kt
+++ src/test/kotlin/gongback/pureureum/application/FacilityServiceTest.kt
@@ -1,9 +1,12 @@
 package gongback.pureureum.application
 
+import gongback.pureureum.domain.facility.FacilityProgress
 import gongback.pureureum.domain.facility.FacilityRepository
-import gongback.pureureum.domain.facility.getApprovedByCategory
+import gongback.pureureum.domain.facility.getAllNotApprovedByCategory
+import gongback.pureureum.domain.facility.getApprovedByCategoryAndUserId
 import gongback.pureureum.domain.facility.getByUserId
 import gongback.pureureum.domain.facility.getDocFileKeyByDocId
+import gongback.pureureum.domain.facility.getFacilityById
 import gongback.pureureum.domain.user.UserRepository
 import gongback.pureureum.domain.user.getUserByEmail
 import io.kotest.assertions.throwables.shouldNotThrowAnyUnit
@@ -11,15 +14,19 @@ import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
+import io.mockk.runs
 import support.CERTIFICATION_DOC_FILE_KEY
 import support.CERTIFICATION_DOC_NAME
 import support.CERTIFICATION_DOC_TYPE
 import support.FACILITY_CATEGORY
+import support.FACILITY_PROGRESS
 import support.createFacility
 import support.createFacilityReq
 import support.createFacilityRes
 import support.createFacilityResWithProgress
+import support.createFacilityWithDocIds
 import support.createMockCertificationDoc
 import support.createUser
 
@@ -115,22 +122,22 @@ class FacilityServiceTest : BehaviorSpec({
         val facilities = listOf(createFacility())
         val facilityRes = listOf(createFacilityRes(id = 0L))
 
-        When("올바르지 않은 카테고리 정보라면") {
-            val invalidCategory = "invalidCategory"
-
-            Then("예외가 발생한다") {
-                shouldThrow<PureureumException> { facilityService.getApprovedFacilityByCategory(email, invalidCategory) }
-            }
-        }
-
-        When("올바른 카테고리 정보라면") {
+        When("올바른 카테고리라면") {
             every { userRepository.getUserByEmail(any()) } returns user
-            every { facilityRepository.getApprovedByCategory(category, user.id) } returns facilities
+            every { facilityRepository.getApprovedByCategoryAndUserId(category, user.id) } returns facilities
             every { uploadService.validateFileName(any()) } returns "OriginalFilename"
             every { uploadService.getImageType(any()) } returns "image/png"
 
             Then("사용자의 카테고리별 시설 정보를 반환한다.") {
                 facilityService.getApprovedFacilityByCategory(email, category.name) shouldBe facilityRes
+            }
+        }
+
+        When("올바르지 않은 카테고리라면") {
+            val invalidCategory = "invalidCategory"
+
+            Then("예외가 발생한다") {
+                shouldThrow<PureureumException> { facilityService.getApprovedFacilityByCategory(email, invalidCategory) }
             }
         }
     }
@@ -159,13 +166,85 @@ class FacilityServiceTest : BehaviorSpec({
         val fileUrl = "sampleURL"
 
         When("올바른 인증 서류 아이디라면") {
-            every { facilityRepository.getDocFileKeyByDocId(any()) } returns fileKey
+            every { facilityRepository.getDocFileKeyByDocId(any(), any()) } returns fileKey
             every { uploadService.getFileUrl(any()) } returns fileUrl
             every { uploadService.validateFileName(any()) } returns "OriginalFilename"
             every { uploadService.getImageType(any()) } returns "image/png"
 
             Then("인증 서류의 다운로드 URL을 반환한다") {
-                facilityService.getCertificationDocDownloadPath(docId) shouldBe fileUrl
+                facilityService.getCertificationDocDownloadPath(1L, docId) shouldBe fileUrl
+            }
+        }
+    }
+
+    Given("시설 카테고리") {
+        val facilities = listOf(createFacility(progress = FacilityProgress.NOT_APPROVED))
+        val facilitiesRes = listOf(createFacilityRes())
+
+        When("올바른 카테고리라면") {
+            every { facilityRepository.getAllNotApprovedByCategory(any()) } returns facilities
+
+            Then("해당하는 카테고리의 승인받지 않은 시설 리스트를 반환한다") {
+                facilityService.getNotApprovedFacilitiesByCategory(FACILITY_CATEGORY.name) shouldBe facilitiesRes
+            }
+        }
+
+        When("올바르지 않은 카테고리라면") {
+            val invalidCategory = "invalidCategory"
+
+            Then("예외가 발생한다") {
+                shouldThrow<PureureumException> { facilityService.getNotApprovedFacilitiesByCategory(invalidCategory) }
+            }
+        }
+    }
+
+    Given("시설 아이디") {
+        val facility = createFacility()
+        val facilityWithDocIds = createFacilityWithDocIds()
+
+        When("올바른 시설 아이디라면") {
+            every { facilityRepository.getFacilityById(any()) } returns facility
+
+            Then("시설 정보를 반환한다") {
+                facilityService.getFacilityById(1L) shouldBe facilityWithDocIds
+            }
+        }
+
+        When("올바르지 않은 시설 아이디라면") {
+            every { facilityRepository.getFacilityById(any()) } throws IllegalArgumentException("시설 정보가 존재하지 않습니다")
+
+            Then("예외가 발생한다") {
+                shouldThrow<IllegalArgumentException> { facilityService.getFacilityById(2L) }
+            }
+        }
+    }
+
+    Given("시설 아이디와 진행 상태") {
+        val facility = createFacility()
+
+        When("올바른 시설 아이디와 진행 상태라면") {
+            every { facilityRepository.getFacilityById(any()) } returns facility
+
+            Then("시설 정보가 업데이트된다") {
+                shouldNotThrowAnyUnit { facilityService.updateFacilityProgress(1L, FACILITY_PROGRESS) }
+            }
+        }
+
+        When("올바르지 않은 시설 아이디라면") {
+            every { facilityRepository.getFacilityById(any()) } throws IllegalArgumentException("시설 정보가 존재하지 않습니다")
+
+            Then("예외가 발생한다") {
+                shouldThrow<IllegalArgumentException> { facilityService.updateFacilityProgress(1L, FACILITY_PROGRESS) }
+            }
+        }
+    }
+
+    Given("시설 아이디 리스트와 진행 상태") {
+        When("올바른 시설 아이디 리스트와 진행 상태라면") {
+            every { facilityRepository.updateProgressByIds(any(), any()) } just runs
+
+            Then("시설 정보가 업데이트된다") {
+                shouldNotThrowAnyUnit { facilityService.updateFacilitiesProgress(listOf(1L, 2L), FACILITY_PROGRESS) }
             }
         }
     }
