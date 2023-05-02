4949dcc6b65fdbf0a5be49e3fd79a91e6eab5e7b
YC <gks54410@gmail.com>
2023-05-02 22:19:59 +0900

    PRR-56 [test]: 메인 페이지의 프로젝트 페이지 조회 테스트

--- src/test/kotlin/gongback/pureureum/application/ProjectServiceTest.kt
+++ src/test/kotlin/gongback/pureureum/application/ProjectServiceTest.kt
@@ -1,11 +1,13 @@
 package gongback.pureureum.application
 
 import gongback.pureureum.domain.facility.FacilityRepository
+import gongback.pureureum.domain.facility.getFacilityById
 import gongback.pureureum.domain.project.ProjectRepository
 import gongback.pureureum.domain.project.getProjectById
 import gongback.pureureum.domain.user.UserRepository
 import gongback.pureureum.domain.user.getUserByEmail
-import gongback.pureureum.support.enum.FileType
+import gongback.pureureum.support.constant.Category
+import gongback.pureureum.support.constant.FileType
 import io.kotest.assertions.throwables.shouldNotThrowAnyUnit
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
@@ -14,11 +16,18 @@ import io.mockk.every
 import io.mockk.just
 import io.mockk.mockk
 import io.mockk.runs
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.Pageable
+import support.PROJECT_CATEGORY
+import support.PROJECT_THUMBNAIL_KEY
+import support.SEARCH_TYPE_POPULAR
+import support.createDifferentCategoryProject
 import support.createFacility
 import support.createMockProjectFile
 import support.createProject
 import support.createProjectRegisterReq
 import support.createProjectResWithoutPayment
+import support.createSameCategoryProject
 import support.createUser
 
 class ProjectServiceTest : BehaviorSpec({
@@ -35,7 +44,11 @@ class ProjectServiceTest : BehaviorSpec({
 
         When("프로젝트 정보가 유효하고, 프로젝트 사진들이 없을 경우") {
             val email = user.email
-            val project = projectRegisterReq.toEntityWithInfo(userId = user.id, facilityId = 0L)
+            val project = projectRegisterReq.toEntityWithInfo(
+                userId = user.id,
+                facilityId = 0L,
+                projectCategory = Category.FARMING_HEALING
+            )
             every { userRepository.getUserByEmail(any()) } returns user
             every { projectRepository.save(any()) } returns project
 
@@ -46,7 +59,11 @@ class ProjectServiceTest : BehaviorSpec({
 
         When("프로젝트 정보가 유효하고, 프로젝트 사진들이 있을 경우") {
             val email = user.email
-            val project = createProjectRegisterReq().toEntityWithInfo(userId = user.id, facilityId = 0L)
+            val project = createProjectRegisterReq().toEntityWithInfo(
+                userId = user.id,
+                facilityId = 0L,
+                projectCategory = Category.FARMING_HEALING
+            )
             val projectFileList = listOf(
                 createMockProjectFile("THUMBNAIL", "test1", "image/png", "sample"),
                 createMockProjectFile("COMMON", "test2", "image/png", "sample")
@@ -113,6 +130,52 @@ class ProjectServiceTest : BehaviorSpec({
 
             Then("예외 발생") {
                 shouldThrow<PureureumException> { projectService.deleteProject(projectId, email) }
+            }
+        }
+    }
+
+    Given("검색 조건, 카테고리, Pageable") {
+        val searchType = SEARCH_TYPE_POPULAR
+        val pageable = Pageable.ofSize(10)
+
+        When("카테고리가 null인 경우") {
+            val category = null
+            val projects = createDifferentCategoryProject()
+            val facility = createFacility()
+
+            every {
+                projectRepository.getRunningProjectPartsByTypeAndCategory(
+                    searchType,
+                    category,
+                    pageable
+                )
+            } returns PageImpl(projects, pageable, projects.size.toLong())
+
+            every { facilityRepository.getFacilityById(any()) } returns facility
+
+            every { uploadService.getFileUrl(any()) } returns PROJECT_THUMBNAIL_KEY
+
+            Then("다른 카테고리의 프로젝트 Page 조회 성공") {
+                projectService.getRunningProjectPartsByTypeAndCategory(searchType, category, pageable).size shouldBe 3
+            }
+        }
+
+        When("카테고리가 null이 아닌 경우") {
+            val category = PROJECT_CATEGORY
+            val projects = createSameCategoryProject()
+            val facility = createFacility()
+
+            every {
+                projectRepository.getRunningProjectPartsByTypeAndCategory(searchType, category, pageable)
+            } returns PageImpl(projects, pageable, projects.size.toLong())
+
+            every { facilityRepository.getFacilityById(any()) } returns facility
+
+            every { uploadService.getFileUrl(any()) } returns PROJECT_THUMBNAIL_KEY
+
+            Then("같은 카테고리의 프로젝트 Page 조회 성공") {
+                projectService.getRunningProjectPartsByTypeAndCategory(searchType, category, pageable)
+                    .projectList[0].projectCategory shouldBe category
             }
         }
     }
--- src/test/kotlin/gongback/pureureum/domain/project/ProjectRepositoryTest.kt
+++ src/test/kotlin/gongback/pureureum/domain/project/ProjectRepositoryTest.kt
@@ -1,10 +1,13 @@
 package gongback.pureureum.domain.project
 
+import gongback.pureureum.support.constant.Category
+import gongback.pureureum.support.constant.SearchType
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.ExpectSpec
 import io.kotest.extensions.spring.SpringTestExtension
 import io.kotest.extensions.spring.SpringTestLifecycleMode
 import io.kotest.matchers.shouldBe
+import org.springframework.data.domain.Pageable
 import support.createProject
 import support.test.BaseTests.RepositoryTest
 
@@ -25,6 +28,30 @@ class ProjectRepositoryTest(
 
         expect("프로젝트가 존재하지 않는다") {
             shouldThrow<IllegalArgumentException> { projectRepository.getProjectById(100L) }
+        }
+    }
+
+    context("카테고리에 따른 프로젝트 조회") {
+        val project1 = createProject()
+        val project2 = createProject()
+        val project3 = createProject()
+        val project4 = createProject(category = Category.ETC)
+        projectRepository.saveAll(listOf(project1, project2, project3, project4))
+
+        expect("카테고리가 null일 경우 모든 프로젝트가 조회된다.") {
+            val category = null
+            val searchType = SearchType.POPULAR
+            val pageable = Pageable.ofSize(10)
+            val result = projectRepository.getRunningProjectPartsByTypeAndCategory(searchType, category, pageable)
+            result.content.size shouldBe 4
+        }
+
+        expect("카테고리가 null이 아닐 경우 해당 프로젝트가 조회된다.") {
+            val category = Category.ETC
+            val searchType = SearchType.POPULAR
+            val pageable = Pageable.ofSize(10)
+            val result = projectRepository.getRunningProjectPartsByTypeAndCategory(searchType, category, pageable)
+            result.content.size shouldBe 1
         }
     }
 })
