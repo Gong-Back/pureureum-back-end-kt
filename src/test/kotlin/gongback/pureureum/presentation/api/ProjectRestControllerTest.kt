02bb6f2aaebc8abc58d0cbab7c1b364697f7b2a0
jiwon <ljwon77@daum.net>
2023-08-09 14:26:04 +0900

    PRR-92 [docs]: 검색 타입에 대한 전역 상수 추가

--- src/test/kotlin/gongback/pureureum/presentation/api/ProjectRestControllerTest.kt
+++ src/test/kotlin/gongback/pureureum/presentation/api/ProjectRestControllerTest.kt
@@ -274,7 +274,7 @@ class ProjectRestControllerTest : ControllerTestHelper() {
             createDocument(
                 "get-page-project-part-success",
                 queryParameters(
-                    parameterWithName("searchType").description("검색 타입 (POPULAR: 인기순, LATEST: 최신순)"),
+                    parameterWithName("searchType").description("검색 타입"),
                     parameterWithName("category").description("카테고리").optional(),
                     parameterWithName("page").description("페이지 값").optional(),
                     parameterWithName("size").description("한 페이지에서 받을 데이터 개수").optional()
@@ -323,7 +323,7 @@ class ProjectRestControllerTest : ControllerTestHelper() {
             createDocument(
                 "get-page-project-part-only-search-type-success",
                 queryParameters(
-                    parameterWithName("searchType").description("검색 타입 (POPULAR: 인기순, LATEST: 최신순)"),
+                    parameterWithName("searchType").description("검색 타입"),
                     parameterWithName("category").description("카테고리").optional(),
                     parameterWithName("page").description("페이지 값").optional(),
                     parameterWithName("size").description("한 페이지에서 받을 데이터 개수").optional()
